212fd5df041f39fde82a56cb2539572c9ac2dee1
Jeremy Honl <jerhon@users.noreply.github.com>
2023-04-14 18:32:39 -0500

    Add movement rules for knights.

---
+++ src/Honlsoft.Chess.Tests/Rules/KnightMoveRuleTests.cs
@@ -0,0 +1,28 @@
+﻿using Honlsoft.Chess.Rules;
+
+namespace Honlsoft.Chess.Tests.Rules; 
+
+public class KnightMoveRuleTests {
+
+
+    [Fact]
+    public void GetCandidateMoves_CenterSquare_ReturnsAllPositions() {
+        FakeChessBoard fakeChessBoard = new FakeChessBoard().AddPiece("Ke4");
+        KnightMoveRule rule = new KnightMoveRule();
+        var moves = rule.GetPossibleMoves(fakeChessBoard, SquareName.Parse("e4"));
+
+        var expected = ChessBoardUtils.GetSquares("c3", "c5", "d6", "d2", "f2", "f6", "g3", "g5");
+        
+        moves.Should().HaveCount(8).And.BeEquivalentTo(expected);
+
+    }
+
+    [Fact]
+    public void GetCandidateMoves_NoKnight_() {
+        FakeChessBoard fakeChessBoard = new FakeChessBoard();
+        var rule = new KnightMoveRule();
+        var moves = rule.GetPossibleMoves(fakeChessBoard, SquareName.Parse("e4"));
+        moves.Should().HaveCount(0);
+    }
+    
+}
---
+++ src/Honlsoft.Chess/Rules/Moves/KnightMoveRule.cs
@@ -0,0 +1,46 @@
+﻿namespace Honlsoft.Chess.Rules; 
+
+public class KnightMoveRule : IMoveRule {
+
+    public bool IsApplicable(IChessBoard chessBoard, SquareName from) {
+        var square = chessBoard.GetSquare(from);
+        return square is { Piece: { Type: PieceType.Knight } };
+    }
+    public SquareName[] GetPossibleMoves(IChessBoard chessBoard, SquareName from) {
+
+        if (chessBoard == null) {
+            throw new ArgumentNullException(nameof(chessBoard));
+        }
+        if (from == null) {
+            throw new ArgumentNullException(nameof(from));
+        }
+
+        var square = chessBoard.GetSquare(from);
+        if (square.Piece == null) {
+            return Array.Empty<SquareName>();
+        }
+        
+        var possibleSquares = new[] {
+            from.Add(2, 1),
+            from.Add(2, -1),
+            from.Add(-2, 1),
+            from.Add(-2, -1),
+            from.Add(1, 2),
+            from.Add(-1, 2),
+            from.Add(1, -2),
+            from.Add(-1, -2)
+        };
+
+        var squares = possibleSquares.Where((squareName) => CanMove(chessBoard, squareName, square.Piece.Color));
+        return squares.Where((s) => s != null).ToArray()!;
+    }
+
+    private bool CanMove(IChessBoard chessBoard, SquareName? squareName, PieceColor color) {
+        if (squareName == null) {
+            return false;
+        }
+        var square = chessBoard.GetSquare(squareName);
+        return square.Piece == null || (square?.Piece?.IsOpponent(color) ?? false);
+    }
+    
+}
