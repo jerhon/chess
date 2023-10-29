6389749080619b24f4c86224d576adc3e0f5db1a
Jeremy Honl <jerhon@users.noreply.github.com>
2023-10-29 18:22:19 -0500

    Add tests areound move parts.

--- src/Honlsoft.Chess.Serialization.Pgn.Tests/PgnReaderTests.cs
+++ src/Honlsoft.Chess.Serialization.Pgn.Tests/PgnReaderTests.cs
@@ -9,9 +9,27 @@ public class PgnReaderTests {
     public void ReadTag_Succeeds(string pgnText, PgnTag expectedResult) {
         var serializer = new PgnReader(new PgnTokenizer(pgnText));
         var actualResult = serializer.ReadTag();
-        actualResult.Should().BeEquivalentTo(expectedResult);
+        actualResult.Should().Be(expectedResult);
     }
 
+    [Theory]
+    [MemberData(nameof(ValidMoveParts))]
+    public void ReadMovePart_Succeeds(string pgnText, PgnMovePart expectedResult) {
+        var serializer = new PgnReader(new PgnTokenizer(pgnText));
+        var actualResult = serializer.ReadMovePart();
+        actualResult.Should().Be(expectedResult);
+    }
+
+    public static TheoryData<string, PgnMovePart> ValidMoveParts {
+        get {
+            TheoryData<string, PgnMovePart> data = new();
+            data.Add("1.", new PgnMoveNumber(1, 1));
+            data.Add("1...", new PgnMoveNumber(1, 3));
+            data.Add("e2e4", new PgnMoveText("e2e4"));
+            return data;
+        }
+    }
+    
     public static TheoryData<string, PgnTag> ValidAttributes {
         get {
             TheoryData<string, PgnTag> data = new TheoryData<string, PgnTag>();
