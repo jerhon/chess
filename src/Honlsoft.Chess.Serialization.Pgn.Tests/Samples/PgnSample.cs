b18e0e00c59630508e0777b243f1c768c466de0e
Jeremy Honl <jerhon@users.noreply.github.com>
2023-10-31 00:30:59 -0500

    Fix unit test on linux.

--- src/Honlsoft.Chess.Serialization.Pgn.Tests/Samples/PgnSample.cs
+++ src/Honlsoft.Chess.Serialization.Pgn.Tests/Samples/PgnSample.cs
@@ -5,7 +5,7 @@ namespace Honlsoft.Chess.Serialization.Pgn.Tests.Samples;
 public class PgnSample {
     public static string Read(string sampleName) {
         string basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
-        basePath = basePath.Replace("file:\\", "");
+        basePath = basePath.Replace("file:\\", "").Replace("file:", "");
         string actualPath = Path.Combine(basePath, "Samples", sampleName);
         
         return File.ReadAllText(actualPath);
