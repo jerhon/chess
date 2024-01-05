3649844502333d16ba4ef7bad1c72c87ee61f2bf
Jeremy Honl <jerhon@users.noreply.github.com>
2024-01-05 00:07:03 -0600

    Trying out wasm

---
+++ src/Honlsoft.Chess.Wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Honlsoft.Chess.Wasm;
+
+
+public partial class MainJS
+{
+
+    public static async Task Main()
+    {
+        if (!OperatingSystem.IsBrowser())
+        {
+            throw new PlatformNotSupportedException("This demo is expected to run on browser platform");
+        }
+        
+        Console.WriteLine("Hello, world!");
+    }
+}
