64c0c6f51c5c8b34299b358fa5ebfa03655b2587
Jeremy Honl <jerhon@user.noreply.github.com>
2024-02-03 23:12:50 -0600

    Missed a few files.

---
+++ src/Honlsoft.Chess.Database/ChessContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Honlsoft.Chess.Database;
+
+public class ChessContext : DbContext
+{
+    public DbSet<Game> Games { get; set; }
+    
+    public DbSet<GamePosition> GamePositions { get; set; }
+    
+    public ChessContext(DbContextOptions<ChessContext> options) : base(options) {  }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.HasKey(e => e.GameId);
+            entity.Property(e => e.GameId).ValueGeneratedOnAdd();
+            entity.HasMany(e => e.Positions).WithOne().HasForeignKey(e => e.GameId);
+        });
+        
+        modelBuilder.Entity<GamePosition>(entity =>
+        {
+            entity.HasKey(e => e.GamePositionId);
+            entity.Property(e => e.GamePositionId).ValueGeneratedOnAdd();
+            entity.HasOne(e => e.Game).WithMany(e => e.Positions).HasForeignKey(e => e.GameId);
+        });
+    }
+}
