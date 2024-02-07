1d6ad3c1d8226d67e3a36a597887a37cfc6ae981
Jeremy Honl <jerhon@user.noreply.github.com>
2024-02-07 00:30:44 -0600

    Fix en-passant capture.

--- src/Honlsoft.Chess/ChessGame.cs
+++ src/Honlsoft.Chess/ChessGame.cs
@@ -51,12 +51,12 @@ public class ChessGame : IChessGame {
         
         DoCastlingRights(fromName);
         
-        DoEnPassantTarget(fromName, toName);
-        
         // Move the piece
         _chessPosition.Move(fromName, toName);
 
         DoEnPassantCapture(fromName, toName);
+        
+        DoEnPassantTarget(fromName, toName);
         
         // If this is a promotion, then promote it
         if (promotionPiece != null) {
